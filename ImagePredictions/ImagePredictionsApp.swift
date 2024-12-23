b67b516077f6d97db0e9c8375f4b515366a25555
Ayushi Raj <ayushiraj2313@gmail.com>
2024-12-23 20:05:24 +0530

    Initial Commit

---
+++ ImagePredictions/ImagePredictionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePredictionsApp.swift
+//  ImagePredictions
+//
+//  Created by Sushant on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ImagePredictionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
